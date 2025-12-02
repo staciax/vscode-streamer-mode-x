867cc0478a8fda9761213aa5d12b50262ca0a86d
STACiA <stacia.dev@gmail.com>
2025-12-02 20:21:16 +0700

    feat: remove commented debug logs for document changes and webview disposal

--- src/editor.ts
+++ src/editor.ts
@@ -134,9 +134,6 @@ export class StreamerModeEditor implements vscode.CustomTextEditorProvider {
             vscode.workspace.onDidChangeTextDocument(
                 (e: vscode.TextDocumentChangeEvent) => {
                     if (e.document.uri.fsPath === document.uri.fsPath) {
-                        // this.logger.debug(
-                        //     `editor: document changed: ${document.uri.fsPath}`
-                        // );
                         updateWebview();
                     }
                 },
@@ -144,9 +141,6 @@ export class StreamerModeEditor implements vscode.CustomTextEditorProvider {
 
         // Make sure we get rid of the listener when our editor is closed.
         webviewPanel.onDidDispose(() => {
-            // this.logger.debug(
-            //     `editor: webview disposed for ${document.uri.fsPath}`
-            // );
             changeDocumentSubscription.dispose();
         });
 
