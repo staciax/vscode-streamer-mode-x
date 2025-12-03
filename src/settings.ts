b4d76f06ca60e928936aae1efe37574a128c2a58
STACiA <stacia.dev@gmail.com>
2025-12-03 17:21:43 +0700

    feat: Add StreamerModeSettings interface and configuration functions

---
+++ src/settings.ts
@@ -0,0 +1,49 @@
+import vscode from 'vscode';
+
+export interface StreamerModeSettings {
+    enabled: boolean;
+    autoDetected: {
+        enable: boolean;
+        interval: {
+            active: number;
+            inactive: number;
+        };
+    };
+}
+
+export function getSettings(): StreamerModeSettings {
+    const config = vscode.workspace.getConfiguration('streamer-mode');
+    return {
+        enabled: config.get<boolean>('enabled', true),
+        autoDetected: {
+            enable: config.get<boolean>('autoDetected.enable', true),
+            interval: {
+                active: config.get<number>('autoDetected.interval.active', 60),
+                inactive: config.get<number>(
+                    'autoDetected.interval.inactive',
+                    30,
+                ),
+            },
+        },
+    };
+}
+
+export function getConfig<T>(
+    section: string,
+    key: string,
+    defaultValue?: T,
+): T {
+    return (
+        vscode.workspace.getConfiguration(section).get<T>(key) ??
+        (defaultValue as T)
+    );
+}
+
+export async function updateConfig(
+    section: string,
+    key: string,
+    value: any,
+    target: vscode.ConfigurationTarget = vscode.ConfigurationTarget.Global,
+) {
+    await vscode.workspace.getConfiguration(section).update(key, value, target);
+}
