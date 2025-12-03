6d76218bd37af80e52fb1c417381952a25a13360
STACiA <stacia.dev@gmail.com>
2025-12-03 20:30:58 +0700

    refactor: Update status bar text to use lock icon for streamer mode

--- src/status-bar.ts
+++ src/status-bar.ts
@@ -20,7 +20,7 @@ export class StatusBar implements vscode.Disposable {
             vscode.StatusBarAlignment.Left,
             -1,
         );
-        this.statusBarItem.text = '$(check) Streamer Mode';
+        this.statusBarItem.text = '$(lock) Streamer Mode';
         this.statusBarItem.command = 'streamer-mode.toggle';
         this.show();
 
@@ -29,7 +29,7 @@ export class StatusBar implements vscode.Disposable {
 
     public update(enable: boolean): void {
         this.statusBarItem.text = enable
-            ? '$(check) Streamer Mode'
+            ? '$(lock) Streamer Mode'
             : '$(x) Streamer Mode';
     }
 
