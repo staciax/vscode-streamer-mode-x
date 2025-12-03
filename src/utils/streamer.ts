7b41cf094437f04d01b313675179dff0ca79db60
STACiA <stacia.dev@gmail.com>
2025-12-03 19:53:16 +0700

    feat: Add detectStreamingApps function to identify active streaming applications

---
+++ src/utils/streamer.ts
@@ -0,0 +1,27 @@
+import find from 'find-process';
+
+const STREAMING_APPS = [
+    'obs',
+    'streamlabs',
+    'xsplit',
+    // add more as needed, must be in lowercase
+];
+
+export async function detectStreamingApps(
+    additionalApps: string[] = [],
+): Promise<boolean> {
+    const allApps = [
+        ...STREAMING_APPS,
+        ...additionalApps.map((app) => app.toLowerCase()),
+    ];
+    try {
+        const processes = await find('name', '');
+
+        return processes.some((proc) =>
+            allApps.some((app) => proc.name.toLowerCase().includes(app)),
+        );
+    } catch (error) {
+        console.log(`Failed to detect: ${error}`);
+        return false;
+    }
+}
