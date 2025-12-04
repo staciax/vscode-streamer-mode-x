f1ce7d46e8c34cad75fef8e42cbe423239f0885c
STACiA <stacia.dev@gmail.com>
2025-12-04 12:50:09 +0700

    refactor: Correct file pattern in test configuration to include all test files

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from '@vscode/test-cli';
 
 export default defineConfig({
-    files: 'out/test/**/*.test.js',
+    files: ['out/*.test.js', 'out/**/*.test.js'],
     launchArgs: ['src/test/workspace'],
 });
