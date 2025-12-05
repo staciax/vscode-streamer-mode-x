d5c45b04181a7548fcca680de0ca250b95b9364b
STACiA <stacia.dev@gmail.com>
2025-12-05 13:57:58 +0700

    refactor: Add coverage configuration to testing setup and update package.json for coverage testing

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -3,4 +3,8 @@ import { defineConfig } from '@vscode/test-cli';
 export default defineConfig({
     files: ['out/*.test.js', 'out/**/*.test.js'],
     launchArgs: ['src/test/workspace'],
+    coverage: {
+        reporter: ['text', 'lcov'],
+        exclude: ['**/src/test/**', '**/node_modules/**'],
+    },
 });
